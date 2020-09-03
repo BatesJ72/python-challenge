ee4c2e4dc9b4a48c1fabfaadc6cdf0f0ffe02cbf
Jessica Bates <bates.jessica72@gmail.com>
2020-09-03 01:38:28 +0000

    All HW problems done

--- PyBank/main.py
+++ PyBank/main.py
@@ -34,25 +34,92 @@ with open(path, "r") as file:
         data_3a.append(int(e[1]))
 #     print(data_3a)
     
-    changes = []
+    changes_3 = []
     for i in range(1, len(data_3a)):
-        last_month = data_3a[i-1]
-        current_month = data_3a[i]
+        last_month_3 = data_3a[i-1]
+        current_month_3 = data_3a[i]
 #     print(current_month)
 #     print(last_month)
     
-        change = current_month - last_month
-        changes.append(change)
+        change_3 = current_month_3 - last_month_3
+        changes_3.append(change_3)
     
-# print(type(change))
-    avg_change = round(sum(changes)/len(changes),2)
+#     print(changes_3)
+    avg_change = round(sum(changes_3)/len(changes_3),2)
 #     print(avg_change)
 
 
 # 4. Greatest increase in profits
+with open(path, "r") as file: 
+    header_4 = file.readline()
+    budget_data_4 = file.readlines()
+    data_4 = [row.strip().split(",") for row in budget_data_4]
+    
+    data_4a = []
+    for e in data_4:
+        data_4a.append(int(e[1]))
+#     print(data_4a)
+    
+    changes_4 = []
+    for i in range(1, len(data_4a)):
+        last_month_4 = data_4a[i-1]
+        current_month_4 = data_4a[i]
+#     print(current_month)
+#     print(last_month)
+    
+        change_4 = current_month_4 - last_month_4
+        changes_4.append(change_4)
+    
+#     print(changes_4)
+
+    greatest_incr = max(changes_4)
+#     print(greatest_incr)
+
+    index = changes_4.index(greatest_incr)
+#     print(index)
+    month_4 = data_4[index + 1]
+    month_4a = month_4[0]
+#     print(month_4a)
+    
+
+# 5. Greatest decrease in profits
+with open(path, "r") as file: 
+    header_5 = file.readline()
+    budget_data_5 = file.readlines()
+    data_5 = [row.strip().split(",") for row in budget_data_5]
+    
+    data_5a = []
+    for e in data_5:
+        data_5a.append(int(e[1]))
+#     print(data_4a)
+    
+    changes_5 = []
+    for i in range(1, len(data_5a)):
+        last_month_5 = data_5a[i-1]
+        current_month_5 = data_5a[i]
+#     print(current_month)
+#     print(last_month)
+    
+        change_5 = current_month_5 - last_month_5
+        changes_5.append(change_5)
+    
+#     print(changes_5)
+
+    greatest_decr = min(changes_5)
+#     print(greatest_decr)
+
+    index = changes_5.index(greatest_decr)
+#     print(index)
+    month_5 = data_5[index + 1]
+    month_5a = month_5[0]
+#     print(month_5a)
+    
+    
 
 
 # Summary statements
 print(f"Total Months: {months}")
 print(f"Total: ${total}")
 print(f"Average Change: ${avg_change}")
+print(f"Greatest Increase in Profits: {month_4a} (${greatest_incr})")
+print(f"Greatest Decrease in Profits: {month_5a} (${greatest_decr})")
